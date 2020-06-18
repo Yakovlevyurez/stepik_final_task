10dfc892bcae67cca0f9889c5c2e6a66fb280fd4
yakovlevyurez <y.yakovlev@norsi-trans.ru>
2020-06-18 17:10:58 +0300

    1. В файле login_page.py реализовал трри проверки(поиск по подстроке в ссылке логина, проверка наличия формы регистрации и проверка наличия формы логина) 2. В файле main_page.py добавил бъект LOGIN_LINK 3. В файле test_main_page.py вынес отдельно ссылку страницы логина и реализовал тесты на (поиск по подстроке в ссылке логина, проверка наличия формы регистрации и проверка наличия формы логина)

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,7 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import MainPageLocators
+from selenium import webdriver
 
 
 class LoginPage(BasePage):
@@ -7,14 +10,21 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+    @staticmethod
+    def should_be_login_url(driverr):
+        assert "login" in driverr.current_url, "wrong login url!"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not presented"
+
+
+# if __name__ == '__main__':
+#     driver = webdriver.Chrome()
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+#     m = driver.get(link)
+#     a = driver.current_url
+#     assert "logi" in a, "wrong login url!"
+#     print(a)
